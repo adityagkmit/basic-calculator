d254560b82dd982d402c8ee17303ef6c42d4b39b
Aditya Singh Rajawat <adityarajawat@gkmit.co>
2024-10-16 11:05:40 +0530

    feat: add swagger

--- src/app.js
+++ src/app.js
@@ -4,11 +4,20 @@ const dotenv = require('dotenv');
 const { connectToDB } = require('./config/database.js');
 const calculatorRoutes = require('./routes/operation.route.js');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger/swagger.yaml'));
+
+
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Connect to MongoDB
 connectToDB();
--- src/routes/operation.route.js
+++ src/routes/operation.route.js
@@ -10,7 +10,8 @@ const router = express.Router();
 
 router.post('/operations', performOperation);
 router.get('/operations', getHistory);
+router.delete('/operation', resetHistory);
 router.delete('/operations/:id', clearHistory);
-router.delete('/operations', resetHistory);
+// router.delete('/operations', resetHistory);
 
 module.exports = router;
